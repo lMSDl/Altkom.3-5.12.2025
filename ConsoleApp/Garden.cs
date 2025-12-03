4727ad6599422c02713a8df4c1c8b458aaa71252
Paweł <pawel@msd.info.pl>
2025-12-03 10:45:01 +0100

    Garden

---
+++ ConsoleApp/Garden.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp
+{
+    internal class Garden
+    {
+        public int Size { get; set; }
+        private ICollection<string> Items { get; } = [];
+
+
+        public Garden(int size)
+        {
+            Size = size;
+        }
+
+        public bool Plant(string item)
+        {
+            if (item is null)
+                throw new ArgumentNullException(nameof(item));
+
+            if (string.IsNullOrWhiteSpace(item))
+            {
+                throw new ArgumentException("Item cannot be empty or whitespace.", nameof(item));
+            }
+
+            if (Items.Count >= Size)
+                return false;
+
+            if (Items.Contains(item))
+            {
+                item += Items.Count(x => x.StartsWith(item)) + 1;
+            }
+
+            Items.Add(item);
+            return true;
+        }
+
+        public IEnumerable<string> GetItems()
+        {
+            return Items.ToList();
+        }
+    }
+}
