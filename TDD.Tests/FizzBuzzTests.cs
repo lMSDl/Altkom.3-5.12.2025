e053d064f3a410837ad2bf4fff1b0e6d5f47192a
Paweł <pawel@msd.info.pl>
2025-12-05 12:29:32 +0100

    TDD

---
+++ TDD.Tests/FizzBuzzTests.cs
@@ -0,0 +1,32 @@
+﻿namespace TDD.Tests
+{
+    public class FizzBuzzTests
+    {
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(3, "Fizz")]
+        [InlineData(5, "Buzz")]
+        [InlineData(6, "Fizz")]
+        [InlineData(15, "FizzBuzz")]
+        public void GetFizzBuzz_AnyNumber_CorrectString(int input, string output)
+        {
+            // Act
+            var result = FizzBuzz.GetFizzBuzz(input);
+            // Assert
+            Assert.Equal(output, result);
+        }
+
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(3, "1 2 Fizz")]
+        [InlineData(15, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz")]
+        public void GetSequence(int count, string exprectedOutput)
+        {
+            //Act
+            var result = FizzBuzz.GetSequence(count);
+
+            //Assert
+            Assert.Equal(exprectedOutput, result);
+        }
+    }
+}
---
+++ TDD/FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿
+namespace TDD
+{
+    public class FizzBuzz
+    {
+        public static string GetFizzBuzz(int number)
+        {
+            string result = string.Empty;
+
+            if (IsFizz(number))
+                result += "Fizz";
+            if (IsBuzz(number))
+                result += "Buzz";
+
+            return string.IsNullOrEmpty(result) ? number.ToString() : result;
+        }
+
+        public static string GetSequence(int count)
+        {
+            return string.Join(" ",
+                Enumerable.Range(1, count)
+                .Select(GetFizzBuzz));
+        }
+
+        private static bool IsBuzz(int number)
+        {
+            return number % 5 == 0;
+        }
+
+        private static bool IsFizz(int number)
+        {
+            return number % 3 == 0;
+        }
+    }
+}
+
